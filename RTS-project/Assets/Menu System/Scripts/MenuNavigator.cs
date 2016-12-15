41f153bec6590dbf22855119eb7bed09412d9f33
Xavier Mahiques <xamasif@gmail.com>
2016-12-15 20:09:56 +0100

    Create MenuNavigator object on every scene to move between different Menu. Code MenuNavigator behaviour script

---
+++ RTS-project/Assets/Menu System/Scripts/MenuNavigator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigator : MonoBehaviour {
+
+	// Change between menu screens
+    public void LoadScene(string name) {
+        Debug.Log("Loading new Scene");
+        SceneManager.LoadScene(name);
+	}
+
+}
